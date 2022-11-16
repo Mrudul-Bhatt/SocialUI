8c8ed5e11488786d2d9fda884c3eaf536dbbf8af
Mrudul-Bhatt <mrudul1203bhatt@gmail.com>
2022-11-16 18:54:54 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'SocialUI';
+export class AppComponent implements OnInit {
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    // this.http.get('https://localhost:5001/api/users').subscribe(
+    //   (response) => {
+    //     console.log(response);
+    //     this.users = response;
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   }
+    // );
+
+    this.http.get('https://localhost:7044/api/Users').subscribe({
+      next: (response) => {
+        console.log(response);
+        this.users = response;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
